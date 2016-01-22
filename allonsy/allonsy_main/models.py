860baeeb7f902c38218aeb6c2f8b0d345ac2d6f6
moscam <marc.mosca@gmail.com>
2016-01-21 23:25:39 -0500

    EOD 21 Jan 2016

--- allonsy/allonsy_main/models.py
+++ allonsy/allonsy_main/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+class UserExtension(models.Model):
+    STUDENT = 'STU'
+    STUDENTSTAFF = 'RAS'
+    STAFF = 'STA'
+    STAFFADMIN = 'ADM'
+    ITSERVICE = 'ITS'
+    ITDEVELOPER = 'ITD'
+    ALLONSYDEVELOPER = 'XAD'
+    ALLONSYADMIN = 'XAA'
+    ALLONSYSUPERUSER = 'XAS'
+
+    user_role_choices = (
+        (STUDENT, 'Student'),
+        (STUDENTSTAFF, 'Student Staff'),
+        (STAFF, 'Staff'),
+        (STAFFADMIN, 'Staff Admin'),
+        (ITSERVICE, 'IT Service'),
+        (ITDEVELOPER, 'IT Development'),
+        (ALLONSYDEVELOPER, 'Allonsy Developer'),
+        (ALLONSYADMIN, 'Allonsy Admin'),
+        (ALLONSYSUPERUSER, 'Allonsy Superuser'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_user = models.PositiveIntegerField(primary_key=True)
+    user_role = models.CharField(max_length=3,
+                                 choices=user_role_choices,
+                                 default=STUDENT)
+
+    class Choice(models.Model):
+        def __str__(self):
+            return self.id_user
+
+
+class MeepleBasic (models.Model)
+    id_user = models.OneToOneField(UserExtension.id_user, on_delete=models.CASCADE)
